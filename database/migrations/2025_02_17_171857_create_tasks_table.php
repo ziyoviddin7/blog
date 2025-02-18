0989d8f3a56556f2268915a502498fdf71200587
Ziyoviddin Mamadosimov <univerziyo@gmail.com>
2025-02-18 20:30:56 +0400

    add columns: task, category

--- database/migrations/2025_02_17_171857_create_tasks_table.php
+++ database/migrations/2025_02_17_171857_create_tasks_table.php
@@ -13,7 +13,13 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('description');
             $table->timestamps();
+            $table->softDeletes();
+
+            $table->unsignedBigInteger('category_id');
+
         });
     }
 
